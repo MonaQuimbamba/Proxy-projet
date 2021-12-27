89f1f1193756c9a0567c98111ca19787633c609d
mona-quimamba <claudio.antonio@etu.unilim.fr>
2021-12-27 23:59:02 +0100

    methode GET done

--- proxyServer.py
+++ proxyServer.py
@@ -4,7 +4,6 @@ import sys,os,socket,select,ssl,re
 from thread import *
 import threading
 
-#print_lock = threading.Lock()
 
 """
   Fonction qui permet de lire les données d'une socket
@@ -32,10 +31,6 @@ def read_line(ma_socket):
   - le numéro de port s'il est indiqué ( par défault le port 80 )
 """
 def getHost(ligne):
-    re_get_host= re.compile(r'Host:([\s\S]*)$') # regex pour trouver l'host dans une requete
-    #host = re_get_host.search(ligne)
-    #if host:
-    #    return host.groups(1)
     port=80
     host=""
     for item in ligne.splitlines():
@@ -56,15 +51,15 @@ def getHost(ligne):
    Fonction qui permet de faire un client :
    -  pour se connecter au serveur web
 """
-def makeClient(host,port,request,nouvelle_connexion_navigateur):
+def client(host,port,request,nouvelle_connexion_navigateur):
+    try:
+        adresse_serveur = socket.gethostbyname(host)
+    except Exception as e:
+        print ("Probleme dans le host ", e.args)
+        sys.exit(1)
 
-    adresse_serveur = socket.gethostbyname(host)
-    context = ssl.create_default_context()
     ma_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    if int(port)==80:
-        s_sock=ma_socket
-    else:
-        s_sock = context.wrap_socket(ma_socket, server_hostname=host)
+    s_sock=ma_socket
 
     print(" Client Starting ..... ")
     try:
@@ -73,54 +68,19 @@ def makeClient(host,port,request,nouvelle_connexion_navigateur):
         print ("Probleme de connexion", e.args)
         sys.exit(1)
 
-    check_cache=isCache(host,request)
-
-    if check_cache==False: #s'il n'existe pas un cache du serveur web
-        sendRequest(s_sock,request) # envoyer la requête du client au serveur web
+    s_sock.sendall(request.encode())# envoyer la requête du client au serveur web
     while 1:
-        if check_cache==False:
-            contenu =read_line(s_sock)
-            if contenu:
-                saveRessource(host,contenu) # sauvegarder le contenu du serveur web
-                # envoyer la response au navigateur
-                #nouvelle_connexion_navigateur.sendall(contenu)
+        response = read_line(s_sock)
+        if (len(response) > 0):
+            nouvelle_connexion_navigateur.send(response)
         else:
-            contenuCache=getRessource(host)
-            filename = "ressources/"+host
-            f = open(filename, 'rb')
-            l = f.read(1024)
-            while l:
-               nouvelle_connexion_navigateur.send(l)
-            print('Sent ', repr(l))
-            l = f.read(1024)
-            f.close()
-            print_lock.release()
+            break
     s_sock.close()
-"""
-    Fonction qui envoit une requête au serveur web
-"""
-def sendRequest(socket_client,request):
-    socket_client.sendall(request.encode())
-
-"""
-    Fonction qui récupere les infos venant du  serveur web
-"""
-
-def serverResponse(socket_client):
-    return socket_client.recv(1024)
-
-
-"""
-    Fonction va renvoyer les infos en réponse au navigateur web
-"""
-def sendServerResponse():
-    print("")
-
+    nouvelle_connexion_navigateur.close()
 
 """
         Fonction qui permet de garder une ressource
 """
-
 def saveRessource(host,contenu):
     file = open("ressources/"+host, "w")
     file.write(contenu)
@@ -149,17 +109,13 @@ def isCache(host,request):
             return True
         return False
 
-
-
-
-
 """
    Fonction qui permet de preparer la requete
 
   - supprimer les lignes commençant par : Connection: Keep-Alive et Proxy-Connection: Keep-Alive
   - supprimer la ligne commençant par Accept-Encoding: gzip
 """
-def makeRequest(ligne):
+def request(ligne):
     liste=ligne.splitlines()
     ## faire la 1ere ligne
     res=""
@@ -167,10 +123,13 @@ def makeRequest(ligne):
         res+="GET / HTTP/1.0\r\n"
 
     if liste[0].split(" ")[0]=="CONNECT":
-        res+="GET / HTTP/1.0\r\n"
+        #res+="GET / HTTP/1.0\r\n"
+        print(" Le proxy traite que les requêtes HTPP ")
+        sys.exit(1)
 
     if liste[0].split(" ")[0]=="POST":
-        res+="POST / HTTP/1.0\r\n"
+        print(ligne)
+        #res+="POST / HTTP/1.0\r\n"
     # faire l'en-tete
     for i in range(1,len(liste)):
         if liste[i].split(":")[0] not in ["Connection","Proxy-Connection","Accept-Encoding"]:
@@ -183,6 +142,18 @@ def makeRequest(ligne):
     return res
 
 
+def lancerClient(nouvelle_connexion_navigateur,contenu):
+    try:
+        contenu=contenu.decode()  # voir le contenu
+        host,port=getHost(contenu) # récuperer le host et port
+        requete=request(contenu) # preparer la requête
+        client(host,port,requete,nouvelle_connexion_navigateur) # démarrer le client
+    except Exception as e:
+        print ("Probleme dans ", e.args)
+        sys.exit(1)
+
+
+
 """ === main == """
 ma_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM,socket.IPPROTO_TCP)
 ma_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
@@ -190,50 +161,13 @@ ma_socket.bind(('127.0.0.1', 8080))
 ma_socket.listen(socket.SOMAXCONN)
 print(" Server listening ..... ")
 while 1:
-    (nouvelle_connexion_navigateur, TSAP_depuis) = ma_socket.accept()
-    print ("Nouvelle connexion depuis ", TSAP_depuis)
-    ligne = nouvelle_connexion_navigateur.recv(1024)
-    if  ligne:
-        ligne=ligne.decode()
-        host,port=getHost(ligne)
-        request=makeRequest(ligne)
-
-        ## Faire le client
-        #print(" la request  : ",request)
-        adresse_serveur = socket.gethostbyname(host)
-        context = ssl.create_default_context()
-        ma_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        if int(port)==80:
-            s_sock=ma_socket
-        else:
-            s_sock = context.wrap_socket(ma_socket, server_hostname=host)
-
-        print(" Client Starting ..... ")
-        try:
-            s_sock.connect((adresse_serveur, int(port)))
-        except Exception as e:
-            print ("Probleme de connexion", e.args)
-            sys.exit(1)
-
-        check_cache=isCache(host,request)
-
-        if check_cache==False: #s'il n'existe pas un cache du serveur web
-            sendRequest(s_sock,request) # envoyer la requête du client au serveur web
-        while 1:
-            if check_cache==False:
-                contenu =read_line(s_sock)
-                if contenu:
-                    saveRessource(host,contenu) # sauvegarder le contenu du serveur web
-                    # envoyer la response au navigateur
-                    #nouvelle_connexion_navigateur.sendall(contenu)
-            else:
-                contenuCache=getRessource(host)
-                # envoyer la responde au naviagteur 
-                #nouvelle_connexion_navigateur.sendall(contenu)
-
-        #ma_socket.close()
-        #print(repr(request))
-
+    try:
+        (nouvelle_connexion_navigateur, TSAP_depuis) = ma_socket.accept()
+        print ("Nouvelle connexion depuis ", TSAP_depuis)
+        contenu = nouvelle_connexion_navigateur.recv(9092)
+        start_new_thread(lancerClient,((nouvelle_connexion_navigateur,contenu)))
+    except KeyboardInterrupt:
+       ma_socket.close()
+       sys.exit(1)
 
 ma_socket.close()
-#kill -9 $(ps -A | grep python | awk '{print $1}
