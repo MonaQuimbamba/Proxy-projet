34ea92929d0aa3124cd2b2df27e55be5f71eb254
mona-quimamba <claudio.antonio@etu.unilim.fr>
2021-12-30 21:05:27 +0100

    fin la methode get et post du proxy

--- proxyServer.py
+++ proxyServer.py
@@ -1,7 +1,8 @@
 #! /usr/bin/python3
 # coding: utf-8
 import sys,os,socket,select,ssl,re
-from _thread import *
+import threading
+import subprocess
 
 
 
@@ -39,12 +40,12 @@ def getHost(ligne):
                 # ajouter www dans le host quand y'a pas
                 host=item.split(":")[1]
                 if host.split(".")[0].replace(" ","") not in ["www","Www","WWW","wWw","wwW"]:
-                    host="www."+host.replace(" ","")
+                    host="http://"+host.replace(" ","")
                 port=item.split(":")[2]
             else:
                 host=item.split(":")[1]
                 if host.split(".")[0].replace(" ","") not in ["www","Www","WWW","wWw","wwW"]:
-                    host="www."+host.replace(" ","")
+                    host="http://"+host.replace(" ","")
             return host.replace(" ",""),port
 
 """
@@ -52,16 +53,24 @@ def getHost(ligne):
    -  pour se connecter au serveur web
 """
 def client(host,port,request,nouvelle_connexion_navigateur):
-    try:
-        adresse_serveur = socket.gethostbyname(host)
-    except Exception as e:
-        print ("Probleme dans le host ", e.args)
-        sys.exit(1)
+
+    cmd_ext = subprocess.Popen('host -t a '+host, stdin=subprocess.PIPE, stdout=subprocess.PIPE,shell=True)
+    (sortie_standard,sortie_erreur) = cmd_ext.communicate()
+    if (cmd_ext.returncode != 0): # on teste la valeur de succes de la commande
+        try:
+             host = "www."+host.split("//")[1]
+             adresse_serveur = socket.gethostbyname(host)
+        except Exception as e:
+            print ("Probleme dans le host ", e.args)
+            sys.exit(1)
+    else :
+        adresse_serveur=sortie_standard.split(" ")[-1].split("\n")[0]
+
 
     ma_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s_sock=ma_socket
 
-    print(" Client Starting ..... ")
+    #print(" Client Starting ..... ")
     try:
         s_sock.connect((adresse_serveur, int(port)))
     except Exception as e:
@@ -72,42 +81,11 @@ def client(host,port,request,nouvelle_connexion_navigateur):
     while 1:
         response = read_line(s_sock)
         if (len(response) > 0):
-            nouvelle_connexion_navigateur.send(response)
+            nouvelle_connexion_navigateur.sendall(response)
         else:
             break
     s_sock.close()
     nouvelle_connexion_navigateur.close()
-
-"""
-        Fonction qui permet de garder une ressource
-"""
-def saveRessource(host,contenu):
-    file = open("ressources/"+host, "w")
-    file.write(contenu)
-    file.close()
-
-"""
-    Focntion qui permet de récuperer une ressource qui est dans le cache
-"""
-def getRessource(host):
-    if os.path.isfile("ressources/"+host):
-        try:
-            file = open("ressources/"+host)
-        except Exception as e:
-            print(e.args)
-            sys.exit(1)
-        contenu = file.readline()
-        file.close()
-        return contenu
-
-"""
-    Fonction qui permet de vérifier s'il existe un cache de la requête
-"""
-def isCache(host,request):
-    if request.split("\r\n")[0]=="GET / HTTP/1.0": # si c la 1ere requête GET
-        if os.path.isfile("ressources/"+host): # si il existe un cache
-            return True
-        return False
 
 """
    Fonction qui permet de preparer la requete
@@ -115,21 +93,19 @@ def isCache(host,request):
   - supprimer les lignes commençant par : Connection: Keep-Alive et Proxy-Connection: Keep-Alive
   - supprimer la ligne commençant par Accept-Encoding: gzip
 """
-def request(ligne):
+def request(ligne,host):
     liste=ligne.splitlines()
     ## faire la 1ere ligne
     res=""
     if liste[0].split(" ")[0]=="GET":
-        res+="GET / HTTP/1.0\r\n"
+        res+=liste[0].split(" ")[0]+" "+liste[0].split(" ")[1]+" HTTP/1.0\r\n"
 
     if liste[0].split(" ")[0]=="CONNECT":
-        #res+="GET / HTTP/1.0\r\n"
         print(" Le proxy traite que les requêtes HTPP ")
         sys.exit(1)
 
     if liste[0].split(" ")[0]=="POST":
-        print(ligne)
-        #res+="POST / HTTP/1.0\r\n"
+        res+=liste[0].split(" ")[0]+" "+liste[0].split(" ")[1]+" HTTP/1.0\r\n"
     # faire l'en-tete
     for i in range(1,len(liste)):
         if liste[i].split(":")[0] not in ["Connection","Proxy-Connection","Accept-Encoding"]:
@@ -148,13 +124,13 @@ def lancerClient(nouvelle_connexion_navigateur,contenu):
     try:
         contenu=contenu.decode()  # voir le contenu
         host,port=getHost(contenu) # récuperer le host et port
-        requete=request(contenu) # preparer la requête
+        #print(repr(contenu))
+        requete=request(contenu,host) # preparer la requête
+
         client(host,port,requete,nouvelle_connexion_navigateur) # démarrer le client
     except Exception as e:
-        print ("Probleme dans ", e.args)
+        print ("Probleme dans la fonction lancerClient", e.args)
         sys.exit(1)
-
-
 
 """ === main == """
 ma_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM,socket.IPPROTO_TCP)
@@ -165,9 +141,11 @@ print(" Server listening ..... ")
 while 1:
     try:
         (nouvelle_connexion_navigateur, TSAP_depuis) = ma_socket.accept()
-        print ("Nouvelle connexion depuis ", TSAP_depuis)
+        #print ("Nouvelle connexion depuis ", TSAP_depuis)
         contenu = nouvelle_connexion_navigateur.recv(9092)
-        start_new_thread(lancerClient,((nouvelle_connexion_navigateur,contenu)))
+        clientStart = threading.Thread(target=lancerClient, args=(nouvelle_connexion_navigateur,contenu))
+        clientStart.start()
+
     except KeyboardInterrupt:
        ma_socket.close()
        sys.exit(1)
